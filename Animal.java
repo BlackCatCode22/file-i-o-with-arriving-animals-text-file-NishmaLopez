a2cf0466969811753213b7a27b763a2a10ad4dce
NishmaLopez <141769707+NishmaLopez@users.noreply.github.com>
2023-09-19 12:19:05 -0700

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,13 @@
+public class Animal {
+    public static int numOfAnimals = 0;
+
+
+
+    String desc;
+    String birthSeason;
+    String color;
+    String weight;
+    String origin01;
+    String origin02;
+
+}
--- Main.java
+++ Main.java
@@ -1,54 +1,106 @@
-import java.io.*;
-
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        System.out.println("\n\n Welcome to File I/O ");
+    // Create the genUniqueIDFunction
 
-        String pathName = "C:\\Users\\BE218\\Downloads\\arrivingAnimals.txt";
-        String pathOutput = "C:\\Users\\BE218\\Downloads\\myNewFile.txt";
-        BufferedReader reader = null;
-        String line = "";
+    // values for theSpecies:
+    // lion
+    String genUniqueID(String theSpecies ) {
+        if (theSpecies.equals("lion")) {
+            // do something
+        }
 
-        //Create a file writer
-        BufferedWriter writer = new BufferedWriter(new FileWriter(pathOutput))
+
+
+    }
+    public static void main(String [] args) {
+
+        System.out.println("Welcome to my Zoo Warmup!");
+
+        // Create an animal object
+        Animal myNewAnimal = new Animal();
+
+        myNewAnimal.desc = " this is my description";
+
+        System.out.println("\n the description of the new animal is: " + myNewAnimal.desc);
+
+        // open the arriving animal text file
+
+        // Define the path to the file
+        String filePath = "C:\\Users\\BE218\\IdeaProjects\\Zoo Warm Up\\files\\arrivingAnimals.txt";
+
+
+        // Create an Arraylist to hold animal objects
+        ArrayList<Animal> animalList = new ArrayList<>();
 
         try {
-            reader = new BufferedReader(new FileReader(pathName));
-            while( (line = reader.readLine()) !=null ) {
-                String[] row  = line.split(",");
+            // Create a FileReader and BufferedReader to read the file
+            FileReader fileReader = new FileReader(filePath);
 
-                for(String i : row) {
-                    System.out.println("\n" + i);
-                    writer.write();
-                }
+            //Create a BufferedReader to efficiently read lines
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+
+
+            // Read and print each line from the file
+            while ((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+                // Split the line into 6 substring
+                String[] myArray = line.split( ",");
+                System.out.println("\n myArray[0] is " + myArray[0]);
+                System.out.println("\n myArray[1] is " + myArray[1]);
+
+                // Create an Animal Object from the Animal Class
+                Animal anyOldAnimal = new Animal();
+                // Increment the static animal count
+                Animal.numOfAnimals++;
+
+
+                // Fill the object's data fields
+                anyOldAnimal.desc = myArray[0];
+                anyOldAnimal.birthSeason = myArray[1];
+                anyOldAnimal.color = myArray[2];
+                anyOldAnimal.weight = myArray[3];
+                anyOldAnimal.origin01 = myArray[4];
+                anyOldAnimal.origin02 = myArray[5];
+
+                // Add the newly created animal object to the array list
+                animalList.add(anyOldAnimal);
 
             }
-        }
-        catch(Exception e) {
+
+            // Close the BufferedReader and FileReader
+            bufferedReader.close();
+            fileReader.close();
+        } catch (IOException e) {
+            // Handle any IOException that may occur (e.g., file not found)
             e.printStackTrace();
-        }
-        finally {
-            reader.close();
+            System.err.println("An error occured while reading the file");
         }
 
-        // Write to a file named: myNewFile.txt
+        //We are done with the text file.
+        // Output the ArrayList!
+        //Output the catList with an item in loop
+        for (Animal animal : animalList) {
+            System.out.println(animal.desc);
+            // Get the species
+            
+            System.out.println(animal.birthSeason);
+            System.out.println(animal.color);
+            System.out.println(animal.weight);
+            System.out.println(animal.origin01);
+            System.out.println(animal.origin02);
+            System.out.println("\n\n");
+        }
 
-        // Create a File Writer.
-        BufferedWriter writer = new BufferedWriter(new FileWriter(pathOutput));
-
-        try {
-            writer.write("This is a line of animals... ");
-            // Write to our file with a for loop
-            for (int i =0; i<10; i++) {
-                writer.write("\n i is: " + i);
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-        finally {
-            writer.close();
-        }
+        System.out.println(" The numebr of animals is: " + Animal.numOfAnimals);
     }
 }
+
+
+
+
